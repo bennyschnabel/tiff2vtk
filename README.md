406d4426bb61222bf2cce660d5b305834149f334
bennyschnabel <bennyschnabel@me.com>
2021-05-06 11:46:31 +0200

    Restructuring of the source code

--- README.md
+++ README.md
@@ -2,15 +2,32 @@
 
 Conversion of multipage *.TIFF files to *.VTK file format.
 
-## Requirements & Installation
+## Requirements
 
+### LibTIFF - TIFF Library and Utilities
+
+Libtiff is a library for reading and writing Tagged Image File Format (abbreviated TIFF) files [WIKIPEDIA](https://en.wikipedia.org/wiki/Libtiff).
+
+Install:
+```
+sudo apt update
+sudo apt install libtiff-dev
+```
 ## Usage
 
 To run this project, compile it like:
 ```
- gcc -Wall -L/usr/lib main.c -ltiff -o main
- ./main
- ```
+gcc -Wall -L/usr/lib main.c -ltiff -o tiff2vtk
+./tiff2vtk **tiff file name** **vtk file name**
+```
+
+Example:
+```
+gcc -Wall -L/usr/lib main.c -ltiff -o tiff2vtk
+./tiff2vtk Knochenprobe2stream.tiff Knochenprobe2stream.vtk
+```
+
+## Additional information
 
 [VTK File Formats](https://vtk.org/wp-content/uploads/2015/04/file-formats.pdf)
 
--- main.c
+++ main.c
@@ -18,11 +18,13 @@
 
 // Function declaration
 int dispHeader(void);
+int dispInformationTIFF(int dims[3], float spcng[3], int spp, int bps, int comp, int pin);
 
 // Variable declaration
 char* fileNameImport;
 char* fileNameExport;
 
+int debug;
 int spp;
 int bps;
 int comp;
@@ -39,23 +41,40 @@ clock_t start, end;
 double cpu_time_used;
 
 
-int main()
+int main(int argc, char** argv)
 {
 	start = clock();
 
 	dispHeader();
 
-	// File name of the *.TIFF file to be imported
-	fileNameImport = "Multi_page24bpp.tif";
-	fileNameImport = "multipage_tiff_example.tif";
-	
-	//fileNameImport = "Knochenprobe2stream.tiff";
-	fileNameExport = "test.vtk";
+	// Debug mode (0 = false, 1 = true)
+	debug = 0;
+
+	switch (debug)
+	{
+	case 0:
+		fileNameImport = argv[1];
+
+		fileNameExport = argv[2];
+		break;
+	case 1:
+		fileNameImport = "Multi_page24bpp.tif";
+		fileNameImport = "multipage_tiff_example.tif";
+		//fileNameImport = "Knochenprobe2stream.tiff";
+
+		fileNameExport = "test.vtk";
+		break;
+	default:
+		fileNameImport = "";
+		
+		fileNameExport = "";
+		break;
+	}
 
 	// Open *.TIFF file
 	TIFF* tif = TIFFOpen(fileNameImport, "r");
 
-	// Check if the *.TIFF file is open 
+	// Check if the *.TIFF file is open
 	if (tif) {
 
 		// Determination of the dimensions of the *.TIFF file		
@@ -69,15 +88,17 @@ int main()
 		
 		if (spcng[0] == spcng[1])
 		{
-			// Equal spacing along x, y, z
+			// Assume equal spacing along x, y, z
 			spcng[2] = spcng[1];
 		}
 		else 
 		{
+			// TODO
 			printf("Error in spacing!");
 			void abort(void);
 		}
 
+		// The unit of measurement for XResolution and YResolution.
 		TIFFGetField(tif, TIFFTAG_RESOLUTIONUNIT, &resunit);
 		printf("Resolution Unit: ");
 		if (resunit == 1)
@@ -95,6 +116,7 @@ int main()
 		}
 		else if (resunit == 3)
 		{
+			// TODO
 			printf("Centimeter\n");
 			
 			for (int i = 0; i < 3; i++)
@@ -108,36 +130,30 @@ int main()
 
 			float spcngUserInput;
 			printf("Enter resolution in pixels/in: ");
-			scanf("%f", &spcngUserInput); 
+			scanf("%f", &spcngUserInput);
 
 			for (int i = 0; i < 3; i++)
 			{
 				spcng[i] = 25.4 / spcngUserInput;
 			}
 		}
-		
-		printf("dims = %d %d %d\n", dims[0], dims[1], dims[2]);
-		printf("spcng = %f %f %f\n", spcng[0], spcng[1], spcng[2]);
-		
-		
+
 		// SamplesPerPixel
 		TIFFGetField(tif, TIFFTAG_SAMPLESPERPIXEL, &spp);
-		printf("SamplesPerPixel = %d\n", spp);
 		
 		// BitsPerSample
 		TIFFGetField(tif, TIFFTAG_BITSPERSAMPLE, &bps);
-		printf("BitsPerSample = %d\n", bps);
 
 		// Compression
 		TIFFGetField(tif, TIFFTAG_COMPRESSION, &comp);
-		printf("Compression = %d\n", comp);
 		
 		// PhotometricInterpretation
 		TIFFGetField(tif, TIFFTAG_PHOTOMETRIC, &pin);
-		printf("PhotometricInterpretation = %d\n", pin);
+
+		dispInformationTIFF(dims, spcng, spp, bps, comp, pin);
 	}
 	
-	
+	// Write *.VTK file
 	FILE *fl_un;
 	fl_un = fopen(fileNameExport, "w+");
 	// Part 1:Header
@@ -160,13 +176,13 @@ int main()
 	fprintf(fl_un, "LOOKUP_TABLE default\n");
 	fclose(fl_un);
 	
-	// check the tif is open
+	// Check if the *.TIFF file is open
 	if (tif) 
 	{
 		int countLoop = 0;
 		// Loop over tiff stream
 		do {
-			//printf("%d\n", countLoop);
+			printf("Image %d of %d scaned\n", countLoop, dims[2] - 1);
 			countLoop++;
 			count = 0;
 			tsize_t scanline;
@@ -200,7 +216,7 @@ int main()
 
 	end = clock();
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("Time: %f seconds \n", cpu_time_used);
+	printf("Time = %f seconds \n", cpu_time_used);
 
 	return 0;
 }
@@ -214,6 +230,111 @@ int dispHeader(void)
 	printf("# High Performance Computing Center Stuttgart (HLRS) #\n");
 	printf("# Date: 2021.05.05                                   #\n");
 	printf("######################################################\n\n");
+
+	return 0;
+}
+
+int dispInformationTIFF(int dims[3], float spcng[3], int spp, int bps, int comp, int pin)
+{
+	// Dimensions
+	printf("Dimensions: x = %d, y = %d, z = %d\n", dims[0], dims[1], dims[2]);
+
+	// Spacing
+	printf("Spacing: x = %f, y = %f, z = %f\n", spcng[0], spcng[1], spcng[2]);
+
+	// The number of components per pixel
+	printf("SamplesPerPixel = %d\n", spp);
+
+	// Number of bits per component
+	printf("BitsPerSample = %d\n", bps);
+
+	// Compression scheme used on the image data
+	printf("Compression = %d ", comp);
+
+	if (comp == 1)
+	{
+		printf("(No compression)\n");
+	}
+	else if (comp == 2)
+	{
+		printf("(CCITT modified Huffman RLE)\n");
+	}
+	else if (comp == 3)
+	{
+		printf("(CCITT Group 3 fax encoding)\n");
+	}
+	else if (comp == 4)
+	{
+		printf("(CCITT Group 4 fax encoding)\n");
+	}
+	else if (comp == 5)
+	{
+		printf("(LZW)\n");
+	}
+	else if (comp == 6)
+	{
+		printf("(JPEG ('old-style' JPEG, later overridden in Technote2))\n");
+	}
+	else if (comp == 7)
+	{
+		printf("(JPEG ('new-style' JPEG))\n");
+	}
+	else if (comp == 32773)
+	{
+		printf("(PackBits compression, aka Macintosh RLE)\n");
+	}
+	else
+	{
+		printf("(Not defined yet)\n");
+	}
+	
+	// The color space of the image data
+	printf("PhotometricInterpretation = %d ", pin);
+
+	if (pin == 0)
+	{
+		printf("(WhiteIsZero. For bilevel and grayscale images: 0 is imaged as white.)\n");
+	}
+	else if (pin == 1)
+	{
+		printf("(BlackIsZero. For bilevel and grayscale images: 0 is imaged as black.)\n");
+	}
+	else if (pin == 2)
+	{
+		printf("(RGB. RGB value of (0,0,0) represents black, and (255,255,255) represents white, assuming 8-bit components.)\n");
+	}
+	else if (pin == 3)
+	{
+		printf("(Palette color. In this model, a color is described with a single component.)\n");
+	}
+	else if (pin == 4)
+	{
+		printf("(Transparency Mask.)\n");
+	}
+	else if (pin == 5)
+	{
+		printf("(Seperated, usually CMYK.)\n");
+	}
+	else if (pin == 6)
+	{
+		printf("(YCbCr)\n");
+	}
+	else if (pin == 8)
+	{
+		printf("(CIE L*a*b*)\n");
+	}
+	else if (pin == 9)
+	{
+		printf("(CIE L*a*b*, alternate encoding also known as ICC L*a*b*)\n");
+	}
+	else if (pin == 10)
+	{
+		printf("(CIE L*a*b*, alternate encoding also known as ITU L*a*b*)\n");
+	}
+	else
+	{
+		printf("(Not defined yet)\n");
+	}
 
 	return 0;
 }
